c1ef776d1306cbb76a2765f80844852e87279ec5
hongkun.zhang <hongkun.zhang@i-soft.com.cn>
2025-04-24 16:55:55 +0800

    sync:补充fvm的代码 f25521a3fdb5a7a349524360f939b7a928bd926a

---
+++ BSWCode/Communication/Common/CDD_FVM/CDD_FVM.h
@@ -0,0 +1,183 @@
+/* PRQA S 3108++ */
+/**
+ * Copyright (C) 2024 Isoft Infrastructure Software Co., Ltd.
+ * SPDX-License-Identifier: LGPL-2.1-only-with-exception OR  LicenseRef-Commercial-License
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; version 2.1.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this library;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * or see <https://www.gnu.org/licenses/>.
+ *
+ * Alternatively, this file may be used under the terms of the Isoft Infrastructure Software Co., Ltd.
+ * Commercial License, in which case the provisions of the Isoft Infrastructure Software Co., Ltd.
+ * Commercial License shall apply instead of those of the GNU Lesser General Public License.
+ *
+ * You should have received a copy of the Isoft Infrastructure Software Co., Ltd.  Commercial License
+ * along with this program. If not, please find it at <https://EasyXMen.com/xy/reference/permissions.html>
+ */
+/* PRQA S 3108-- */
+/*
+********************************************************************************
+**                                                                            **
+**  FILENAME    : CDD_FVM.h                                                       **
+**                                                                            **
+**  Created on  : 2023-10-20                                                  **
+**  Author      : Jian.Jiang                                                  **
+**  Vendor      :                                                             **
+**  DESCRIPTION :                                                             **
+**                                                                            **
+**  SPECIFICATION(S) :   AUTOSAR classic Platform R19-11                      **
+**                                                                            **
+*******************************************************************************/
+
+#ifndef CDD_FVM_H
+#define CDD_FVM_H
+
+/*******************************************************************************
+**                      Include Section                                       **
+*******************************************************************************/
+#include "CDD_FVM_Types.h"
+#if (STD_ON == CDD_FVM_DEV_ERROR_DETECT)
+#include "Det.h"
+#endif
+#include "SecOC.h"
+
+/*******************************************************************************
+**                      Global Symbols                                        **
+*******************************************************************************/
+#define CDD_FVM_MODULE_ID        2099u
+#define CDD_FVM_INSTANCE_ID      0u
+#define CDD_FVM_VENDOR_ID        62u
+#define CDD_FVM_SW_MAJOR_VERSION 1u
+#define CDD_FVM_SW_MINOR_VERSION 0u
+#define CDD_FVM_SW_PATCH_VERSION 6u
+#define CDD_FVM_AR_MAJOR_VERSION 4u
+#define CDD_FVM_AR_MINOR_VERSION 5u
+#define CDD_FVM_AR_PATCH_VERSION 0u
+
+/* Development Errors */
+#define CDD_FVM_E_PARAM_POINTER      0x01u
+#define CDD_FVM_E_UNINIT             0x02u
+#define CDD_FVM_E_INVALID_PDU_SDU_ID 0x03u
+#define CDD_FVM_E_INIT_FAILED        0x07u
+
+/*Service ID*/
+#define CDD_FVM_INIT_ID                   ((uint8)0x01u)
+#define CDD_FVM_DEINIT_ID                 ((uint8)0x02u)
+#define CDD_FVM_GETVERSIONINFO            ((uint8)0x03u)
+#define CDD_FVM_GETRXFRESHNESS            ((uint8)0x04u)
+#define CDD_FVM_GETRXFRESHNESSAUTHDATA    ((uint8)0x05u)
+#define CDD_FVM_GETTXFRESHNESS            ((uint8)0x06u)
+#define CDD_FVM_GETTXFRESHNESSTRUNCDATA   ((uint8)0x07u)
+#define CDD_FVM_SPDUTXCONFIRMATION        ((uint8)0x08u)
+#define CDD_FVM_RXINDICATION_ID           ((uint8)0X09u)
+#define CDD_FVM_SLAVEREQUESTSYNCMSG       ((uint8)0X0Au)
+#define CDD_FVM_RXSECOCVERIFICATIONNOTIFY ((uint8)0X0Bu)
+
+/*******************************************************************************
+**                      Global Data Declaration                               **
+*******************************************************************************/
+extern CONST(CDD_FVM_ConfigType, CDD_FVM_CONST) CDD_FVM_ConfigData;
+
+/*******************************************************************************
+**                      Private Macro Definitions                             **
+*******************************************************************************/
+
+#if (STD_ON == CDD_FVM_DEV_ERROR_DETECT)
+static inline void CDD_FVM_DET_REPORTERROR(uint8 ApiId, uint8 ErrorId)
+{
+    (void)Det_ReportError(CDD_FVM_MODULE_ID, CDD_FVM_INSTANCE_ID, (ApiId), (ErrorId));
+}
+#endif /* STD_ON == CANTP_DEV_ERROR_DETECT */
+
+/* PRQA S 3472 ++ */ /* MISRA Dir 4.9 */
+#if !defined(CDD_FVM_PARA_UNUSED)
+#define CDD_FVM_PARA_UNUSED(x) ((void)(x))
+#endif               /* defined CDD_FVM_PARA_UNUSED */
+/* PRQA S 3472 -- */ /* MISRA Dir 4.9 */
+
+/*******************************************************************************
+**                      Global Data Types                                     **
+*******************************************************************************/
+
+/*******************************************************************************
+**                      Global Data Declaration                               **
+*******************************************************************************/
+
+/*******************************************************************************
+**                      Global Functions                                      **
+*******************************************************************************/
+
+FUNC(void, CDD_FVM_CODE) CDD_FVM_Init(P2CONST(CDD_FVM_ConfigType, AUTOMATIC, CDD_FVM_CFG_CONST) config);
+
+FUNC(void, CDD_FVM_CODE) CDD_FVM_DeInit(void);
+
+FUNC(void, CDD_FVM_CODE)
+CDD_FVM_GetVersionInfo(P2VAR(Std_VersionInfoType, AUTOMATIC, CDD_FVM_APPL_DATA) versioninfo);
+
+FUNC(Std_ReturnType, CDD_FVM_CODE)
+CDD_FVM_GetRxFreshness(
+    uint16 FVMFreshnessValueID,
+    P2CONST(uint8, AUTOMATIC, CDD_FVM_APPL_CONST) FVMTruncatedFreshnessValue,
+    uint32 FVMTruncatedFreshnessValueLength,
+    uint16 FVMAuthVerifyAttempts,
+    /* PRQA S 3432 ++ */ /* MISRA Rule 20.7 */
+    P2VAR(uint8, AUTOMATIC, CDD_FVM_APPL_DATA) FVMFreshnessValue,
+    P2VAR(uint32, AUTOMATIC, CDD_FVM_APPL_DATA) FVMFreshnessValueLength
+    /* PRQA S 3432 -- */ /* MISRA Rule 20.7 */
+);
+
+FUNC(Std_ReturnType, CDD_FVM_CODE)
+CDD_FVM_GetRxFreshnessAuthData(
+    uint16 FVMFreshnessValueID,
+    P2CONST(uint8, AUTOMATIC, CDD_FVM_APPL_CONST) FVMTruncatedFreshnessValue,
+    uint32 FVMTruncatedFreshnessValueLength,
+    P2CONST(uint8, AUTOMATIC, CDD_FVM_APPL_CONST) FVMAuthDataFreshnessValue,
+    uint16 FVMAuthDataFreshnessValueLength,
+    uint16 FVMAuthVerifyAttempts,
+    /* PRQA S 3432 ++ */ /* MISRA Rule 20.7 */
+    P2VAR(uint8, AUTOMATIC, CDD_FVM_APPL_DATA) FVMFreshnessValue,
+    P2VAR(uint32, AUTOMATIC, CDD_FVM_APPL_DATA) FVMFreshnessValueLength
+    /* PRQA S 3432 -- */ /* MISRA Rule 20.7 */
+);
+
+FUNC(Std_ReturnType, CDD_FVM_CODE)
+CDD_FVM_GetTxFreshness(
+    uint16 FVMFreshnessValueID,
+    /* PRQA S 3432 ++*/ /* MISRA Rule 20.7 */
+    P2VAR(uint8, AUTOMATIC, CDD_FVM_APPL_DATA) FVMFreshnessValue,
+    P2VAR(uint32, AUTOMATIC, CDD_FVM_APPL_DATA) FVMFreshnessValueLength
+    /* PRQA S 3432 --*/ /* MISRA Rule 20.7 */
+);
+
+FUNC(Std_ReturnType, CDD_FVM_CODE)
+CDD_FVM_GetTxFreshnessTruncData(
+    uint16 FVMFreshnessValueID,
+    /* PRQA S 3432 ++*/ /* MISRA Rule 20.7 */
+    P2VAR(uint8, AUTOMATIC, CDD_FVM_APPL_CONST) FVMFreshnessValue,
+    P2VAR(uint32, AUTOMATIC, CDD_FVM_APPL_DATA) FVMFreshnessValueLength,
+    P2VAR(uint8, AUTOMATIC, CDD_FVM_APPL_CONST) FVMTruncatedFreshnessValue,
+    P2VAR(uint32, AUTOMATIC, CDD_FVM_APPL_DATA) FVMTruncatedFreshnessValueLength
+    /* PRQA S 3432 --*/ /* MISRA Rule 20.7 */
+);
+
+FUNC(Std_ReturnType, CDD_FVM_CODE) CDD_FVM_SPduTxConfirmation(uint16 FVMFreshnessValueID);
+
+FUNC(void, CDD_FVM_CODE) CDD_FVM_RxSecOCVerificationNotify(SecOC_VerificationStatusType verificationStatus);
+
+FUNC(void, CDD_FVM_CODE)
+CDD_FVM_RxIndication(PduIdType RxPduId, P2CONST(PduInfoType, AUTOMATIC, CDD_FVM_APPL_CONST) PduInfoPtr);
+
+#if (CDD_FVM_MASTER_ECU_ENABLED == STD_ON) && (CDD_FVM_MASTER_ECU_CONFIG_NUM > 0u)
+FUNC(void, CDD_FVM_CODE) CDD_FVM_MainFunctionMaster(void);
+#endif /* CDD_FVM_MASTER_ECU_ENABLED == STD_ON */
+
+#if (CDD_FVM_SLAVE_CTRL_PDU_ENABLED == STD_ON)
+FUNC(Std_ReturnType, CDD_FVM_CODE) CDD_FVM_SlaveRequestSyncMsg(uint16 syncMsgId);
+#endif /* CDD_FVM_SLAVE_CTRL_PDU_ENABLED == STD_ON && CDD_FVM_MASTER_ECU_CONFIG_NUM > 0u*/
+
+#endif /* CDD_FVM_H */
---
+++ BSWCode/Communication/Common/CDD_FVM/CDD_FVM_Internal.h
@@ -0,0 +1,194 @@
+/* PRQA S 3108++ */
+/**
+ * Copyright (C) 2024 Isoft Infrastructure Software Co., Ltd.
+ * SPDX-License-Identifier: LGPL-2.1-only-with-exception OR  LicenseRef-Commercial-License
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; version 2.1.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this library;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * or see <https://www.gnu.org/licenses/>.
+ *
+ * Alternatively, this file may be used under the terms of the Isoft Infrastructure Software Co., Ltd.
+ * Commercial License, in which case the provisions of the Isoft Infrastructure Software Co., Ltd.
+ * Commercial License shall apply instead of those of the GNU Lesser General Public License.
+ *
+ * You should have received a copy of the Isoft Infrastructure Software Co., Ltd.  Commercial License
+ * along with this program. If not, please find it at <https://EasyXMen.com/xy/reference/permissions.html>
+ */
+/* PRQA S 3108-- */
+/*
+********************************************************************************
+**                                                                            **
+**  FILENAME    : CDD_FVM_Internal.h                                          **
+**                                                                            **
+**  Created on  : 2023/10/20                                                  **
+**  Author      : Jian.Jiang                                                  **
+**  Vendor      :                                                             **
+**  DESCRIPTION : FVM internal use                                            **
+**                                                                            **
+**  SPECIFICATION(S) :   AUTOSAR classic Platform R19-11                      **
+**                                                                            **
+*******************************************************************************/
+#ifndef CDD_FVM_INTERNAL_H
+#define CDD_FVM_INTERNAL_H
+
+/*******************************************************************************
+**                      Includes                                              **
+*******************************************************************************/
+#include "CDD_FVM_Cfg.h"
+
+/*******************************************************************************
+**                      Private Macro Definitions                             **
+*******************************************************************************/
+#define CDD_FVM_FRESHNESS_VALUE_MAX_BYTE          (8u)
+
+#define CDD_FVM_TRIP_COUNTER_VALUE_MAX_BYTE       (3u)
+#define CDD_FVM_RESET_COUNTER_VALUE_MAX_BYTE      (3u)
+#define CDD_FVM_MESSAGE_COUNTER_VALUE_MAX_BYTE    (6u)
+
+#define CDD_FVM_SINGLE_FRESHNESS_COUNT_INIT_VALUE (1u)
+
+#define CDD_FVM_E_EQUAL                           (0u)
+#define CDD_FVM_E_NOT_OK                          (1u)
+#define CDD_FVM_E_MORE_THAN                       (111u)
+#define CDD_FVM_E_LESS_THAN                       (112u)
+
+#define CDD_FVM_TRIP_COUNTER_INIT_VALUE           (1u)
+
+/* Define the maximum data statistics that can be received from the slave. */
+#define CDD_FVM_MASTER_RX_SLAVE_DATA_MAX (32u)
+
+/* Define the maximum length of the control frame */
+#define CDD_FVM_CTRL_FRAME_BYTES_MAX (8u)
+
+/* Define the broadcast message for control */
+#define CDD_FVM_CTRL_BROADCAST_SYNMSG_ID (0xFFFF)
+
+/* Define the protocol version of the control frame */
+#define CDD_FVM_CTRL_FRAME_PROTOCOL_VERSION (1u)
+
+/* Define the control message type */
+static inline uint8 CDD_FVM_CLEAR_BITS(uint8 value)
+{
+    return ((value) & ~((1u << 5u) - 1u));
+}
+
+static inline uint8 CDD_FVM_CTRL_MSG_TYPE_SLAVE_VERIFY(uint8 value)
+{
+    return (((value) & ~((1u << 5u) - 1u)) | 1u);
+}
+
+static inline uint8 CDD_FVM_CTRL_MSG_TYPE_MASTER_ASK(uint8 value)
+{
+    return (((value) & ~((1u << 5u) - 1u)) | 2u);
+}
+static inline uint8 CDD_FVM_CTRL_MSG_TYPE_SLAVE_REQUSEST(uint8 value)
+{
+    return (((value) & ~((1u << 5u) - 1u)) | 4u);
+}
+
+#define CDD_FVM_CTRL_MSG_TYPE_VERIFY  (1u)
+#define CDD_FVM_CTRL_MSG_TYPE_ASK     (2u)
+#define CDD_FVM_CTRL_MSG_TYPE_REQUEST (4u)
+
+/* Returns the remainder result, only for powers of 2 where the divisor is */
+static inline uint8 CDD_FVM_MOD_SHIFT(uint8 dividend, uint8 divisor)
+{
+    return ((dividend) & ((divisor)-1u));
+}
+
+/* Returns the remainder form 8 */
+static inline uint8 CDD_FVM_MOD_FROM_EIGHT(uint8 dividend)
+{
+    return ((dividend) & 0X07u);
+}
+
+/* Returns the quotient, only for powers of 2 where the divisor is, y is raised to the power of 2*/
+static inline uint8 CDD_FVM_DIV_SHIFT(uint8 dividend, uint8 y)
+{
+    return ((dividend) >> (y));
+}
+
+static inline uint8 CDD_FVM_BIT_TO_BYTE(uint8 nbits)
+{
+    return ((nbits + 7u) >> 3u);
+}
+
+static inline uint64 CDD_FVM_GET_N_BITS_MAX_VALUE(uint8 nbits)
+{
+    return ((1uLL << (nbits)) - 1u);
+}
+
+/* Macro used to get configuration data */
+
+/* TX*/
+
+/*******************************************************************************
+**                      Private Type Definitions                               *
+*******************************************************************************/
+
+/* For master-slave control frame state switching */
+typedef enum
+{
+    CDD_FVM_CTRL_INIT = 0u, /* initial state */
+    CDD_FVM_CTRL_WAIT,      /* State after sending a request frame */
+    CDD_FVM_CTRL_OK,        /* Status of correct response received */
+    CDD_FVM_CTRL_ERR,       /* Received an incorrect response */
+} CDD_FVM_MultipleCtrlStatusType;
+
+typedef struct
+{
+    uint8 FVMmsgCnt[CDD_FVM_FRESHNESS_VALUE_MAX_BYTE];
+} CDD_FVM_TxFreshnessCntType;
+
+typedef struct
+{
+    uint8 FVMmsgCnt[CDD_FVM_FRESHNESS_VALUE_MAX_BYTE];
+    uint8 FVMmsgRxFreshnessValue[CDD_FVM_FRESHNESS_VALUE_MAX_BYTE];
+} CDD_FVM_RxFreshnessCntType;
+
+typedef struct
+{
+    uint8 FVMFreshnessValue[CDD_FVM_FRESHNESS_VALUE_MAX_BYTE];
+    uint8 FvmMsgCnt[CDD_FVM_MESSAGE_COUNTER_VALUE_MAX_BYTE];
+    uint8 FVMPreviousValue[CDD_FVM_FRESHNESS_VALUE_MAX_BYTE];
+} CDD_FVM_MultipleFreshnessCntType;
+
+typedef struct
+{
+    CDD_FVM_MultipleCtrlStatusType status;
+    uint8 FvMTripCnt[CDD_FVM_TRIP_COUNTER_VALUE_MAX_BYTE];
+    uint8 FvMResetCnt[CDD_FVM_RESET_COUNTER_VALUE_MAX_BYTE];
+#if (CDD_FVM_SLAVE_CTRL_PDU_ENABLED == STD_ON)
+    uint8 FvmCtrlBuffer[CDD_FVM_CTRL_FRAME_BYTES_MAX];
+    uint8 FvmCounter;
+#endif /* CDD_FVM_SLAVE_CTRL_PDU_ENABLED == STD_ON */
+} CDD_FVM_SlaveECUSyncMsgType;
+
+typedef struct
+{
+    uint8 FvmEcuInstanceId;
+    uint16 FvmSyncMsgId;
+    uint8 FvmRxCounter;
+    CDD_FVM_MultipleCtrlStatusType status;
+    boolean isUse;
+} CDD_FVM_MasterRxSlveCtrlInfoType;
+typedef struct
+{
+    CDD_FVM_MultipleCtrlStatusType status;
+    uint8 FvMTripCnt[CDD_FVM_TRIP_COUNTER_VALUE_MAX_BYTE];
+    uint8 FvMResetCnt[CDD_FVM_RESET_COUNTER_VALUE_MAX_BYTE];
+    uint8 FvMSynMsgBuff[CDD_FVM_TRIP_COUNTER_VALUE_MAX_BYTE + CDD_FVM_RESET_COUNTER_VALUE_MAX_BYTE];
+    uint16 resetCycle;
+    boolean isRunSend;
+#if (CDD_FVM_MASTER_CTRL_PDU_ENABLED == STD_ON)
+    CDD_FVM_MasterRxSlveCtrlInfoType slaveRxCtrlData[CDD_FVM_MASTER_RX_SLAVE_DATA_MAX];
+    uint8 FvmCtrlBuffer[CDD_FVM_CTRL_FRAME_BYTES_MAX];
+#endif /* CDD_FVM_MASTER_CTRL_PDU_ENABLED == STD_ON */
+} CDD_FVM_MasterECUSyncMsgType;
+
+#endif /* CDD_FVM_INTERNAL_H */
