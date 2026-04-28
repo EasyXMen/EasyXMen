e4b75a6e0c37ff0b8141ea0abc073bb442c1071e
jiajiang.xie <jiajiang.xie@i-soft.com.cn>
2026-04-28 09:48:18 +0800

    chore: 更新copyright和文件

--- RTOS/Portable/Mcu/NXP/S32K148/inc/Os_Mcu_Timer.h
+++ RTOS/Portable/Mcu/NXP/S32K148/inc/Os_Mcu_Timer.h
@@ -1,14 +1,17 @@
-/*******************************************************************************
-**                                                                            **
-** Copyright (C) iSOFT   (2016-2023)                                          **
-**                                                                            **
-** All rights reserved.                                                       **
-**                                                                            **
-** This document contains proprietary information belonging to iSOFT.         **
-** Passing on and copying of this document, and communication                 **
-** of its contents is not permitted without prior written authorization.      **
-**                                                                            **
-********************************************************************************
+/**
+ * Copyright (C) 2008-2026 isoft Infrastructure Software Co., Ltd.
+ * SPDX-License-Identifier: LGPL-2.1-only-with-exception
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; version 2.1.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this library;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * or see <https://www.gnu.org/licenses/>.
+ */
+/*
 **                                                                            **
 **  FILENAME    :  Os_Mcu_Timer.h                                             **
 **                                                                            **
